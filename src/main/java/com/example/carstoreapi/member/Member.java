7db086915fde0d3c487cb9f87bfab406c6e2b7e2
joo9710 <joo9710@naver.com>
2022-06-27 17:15:29 +0900

    회원가입/로그인기능 구현 끝

--- src/main/java/com/example/carstoreapi/member/Member.java
+++ src/main/java/com/example/carstoreapi/member/Member.java
@@ -1,33 +1,38 @@
 package com.example.carstoreapi.member;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@Setter
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
+    @Column(name = "mid")
+    private Long mid;
 
     @Column(unique = true)
     private String email;
     private String password;
+    private String nickName;
+    private String area;
+    private String phoneNumber;
 
-    public Member(String email, String password) {
+
+    public Member(String email, String password, String nickName, String area , String phoneNumber) {
         this.email = email;
         this.password = password;
+        this.nickName = nickName;
+        this.area = area;
+        this.phoneNumber = phoneNumber;
     }
 
-    public static Member createMember(String email, String password){
-        return new Member(email, password);
+    public static Member createMember(String email, String password, String nickName, String area, String phoneNumber){
+        return new Member(email, password, nickName, area, phoneNumber);
     }
 }
